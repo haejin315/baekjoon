def80f06084f3014a8e8c2cd3abcdf96c2a50299
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-02-23 10:04:33 +0900

    baekjoon step by step

---
+++ baekjoon15(동적 계획법1)/baekjoon15-1(1003_피보나치 함수)/baekjoon15-1(1003_피보나치 함수)/source.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int a[3][2];
+int c = 1;
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    int b, T;
+    cin >> b;
+    for (int d = 0; d < b; d++) {
+        cin >> T;
+        a[0][0] = 1; a[0][1] = 0;
+        a[1][0] = 0; a[1][1] = 1;
+        c = 1;
+        if (T == 0) {
+            cout << "1 0\n";
+            continue;
+        }
+        else if (T == 1) {
+            cout << "0 1\n";
+            continue;
+        }
+        for (int i = 2; i <= T; i++) {
+            c = (c + 1) % 3;
+            a[c][0] = a[(c + 1) % 3][0] + a[(c + 2) % 3][0];
+            a[c][1] = a[(c + 1) % 3][1] + a[(c + 2) % 3][1];
+        }
+        cout << a[c][0] << " " << a[c][1] << "\n";
+    }
+}
+
