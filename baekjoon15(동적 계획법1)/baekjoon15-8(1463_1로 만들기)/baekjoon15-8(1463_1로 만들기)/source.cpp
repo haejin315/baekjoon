99092e9005e90d2d57d6bec496588e2cac43c800
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-03-01 03:46:58 +0900

    baekjoon step by step

---
+++ baekjoon15(동적 계획법1)/baekjoon15-8(1463_1로 만들기)/baekjoon15-8(1463_1로 만들기)/source.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int a[1000001];
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    int N;
+    cin >> N;
+    a[1] = 0;
+    a[2] = 1;
+    for (int i = 3; i <= N; i++) {
+        if (i % 3 == 0 && i % 2 == 0) {
+            if (a[i / 3] <= a[i / 2] && a[i / 3] <= a[i - 1]) {
+                a[i] = a[i / 3] + 1;
+            }
+            else if (a[i / 2] <= a[i - 1]) {
+                a[i] = a[i / 2] + 1;
+            }
+            else {
+                a[i] = a[i - 1] + 1;
+            }
+        }
+        else if (i % 3 == 0) {
+            if (a[i / 3] <= a[i - 1]) {
+                a[i] = a[i / 3] + 1;
+            }
+            else {
+                a[i] = a[i - 1] + 1;
+            }
+        }
+        else if (i % 2 == 0) {
+            if (a[i / 2] <= a[i - 1]) {
+                a[i] = a[i / 2] + 1;
+            }
+            else {
+                a[i] = a[i - 1] + 1;
+            }
+        }
+        else {
+            a[i] = a[i - 1] + 1;
+        }
+    }
+    cout << a[N];
+}
+
