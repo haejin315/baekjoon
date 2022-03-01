d1a0c4d14e583209418b6831f0b78e9e22d51208
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-03-02 05:13:25 +0900

    baekjoon step by step

---
+++ baekjoon15(동적 계획법1)/baekjoon16-9(10844_쉬운 계단 수)/baekjoon16-9(10844_쉬운 계단 수)/source.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int b = 1000000000;
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    int N;
+    cin >> N;
+    int a[100][10] = {0};
+    for (int i = 1; i < 10; i++) {
+        a[0][i] = 1;
+    }
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (j == 0) {
+                a[i][j] = a[i - 1][j + 1];
+            }
+            else if (j == 9) {
+                a[i][j] = a[i - 1][j - 1];
+            }
+            else {
+                a[i][j] = (a[i - 1][j + 1] + a[i - 1][j - 1]) % b;
+            }
+        }
+        
+    }
+    int ans = 0;
+    for (int i = 0; i < 10; i++) {
+        ans = ans % b + a[N - 1][i];
+    }
+    cout << ans % b;
+}
+
