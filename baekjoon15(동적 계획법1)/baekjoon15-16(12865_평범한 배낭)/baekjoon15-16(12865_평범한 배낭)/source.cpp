9e214c7783d7ae43629621a952d6920a34db6418
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-03-09 07:16:00 +0900

    baekjoon step by step

---
+++ baekjoon15(동적 계획법1)/baekjoon15-16(12865_평범한 배낭)/baekjoon15-16(12865_평범한 배낭)/source.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[101][2] = { 0 };
+int b[101][100001] = { 0 };
+
+int main()
+{
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	int N, K;
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) {
+		cin >> a[i][0] >> a[i][1];
+	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= K; j++) {
+			if (j >= a[i][0])
+				b[i][j] = max(b[i - 1][j], (b[i - 1][j - a[i][0]]) + a[i][1]);
+			else
+				b[i][j] = b[i - 1][j];
+		}
+	}
+	cout << b[N][K];
+}
+
