45527e6f75af6a2bd422f9bb2ae62529f0172323
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-03-15 07:10:33 +0900

    baekjoon step by step

---
+++ baekjoon17(정수론 및 조합론)/baekjoon17-2(1037_약수)/baekjoon17-2(1037_약수)/source.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    int N, b;
+    cin >> N;
+    vector<int> a;
+    for (int i = 0; i < N; i++) {
+        cin >> b;
+        a.push_back(b);
+    }
+    sort(a.begin(), a.end());
+    cout << a[0] * a[N - 1];
+}
+
