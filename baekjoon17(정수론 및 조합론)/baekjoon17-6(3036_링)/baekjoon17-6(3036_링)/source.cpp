32096bd2f2cc19a81d3b0835d4ef846cbab4ed2d
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-03-21 11:31:30 +0900

    baekjoon step by step

---
+++ baekjoon17(정수론 및 조합론)/baekjoon17-6(3036_링)/baekjoon17-6(3036_링)/source.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int mindivisor(int big, int small) {
+    int r;
+    if (big < small) {
+        big ^= small;
+        small ^= big;
+        big ^= small;
+    }
+    while (1) {
+        if (big % small == 0) {
+            break;
+        }
+        r = big - small;
+        if (r > small) {
+            big = r;
+        }
+        else {
+            big = small;
+            small = r;
+        }
+    }
+    return small;
+}
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    int N, b;
+    cin >> N;
+    int a[100] = { 0 };
+    cin >> a[0];
+    for (int i = 1; i < N; i++) {
+        cin >> a[i];
+        b = mindivisor(a[0], a[i]);
+        cout << a[0] / b << "/" << a[i] / b << "\n";
+    }
+}
+
