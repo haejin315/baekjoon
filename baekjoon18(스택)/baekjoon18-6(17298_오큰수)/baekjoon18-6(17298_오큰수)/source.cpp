1721727cb97401bb6fb16a012e914293edd3027c
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-04-02 06:30:19 +0900

    baekjoon step by step

---
+++ baekjoon18(스택)/baekjoon18-6(17298_오큰수)/baekjoon18-6(17298_오큰수)/source.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int t[1000001][2] = { 0 };
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    int N;
+    cin >> N;
+    stack<int> a;
+    for (int i = 0; i < N; i++) {
+        cin >> t[i][0];
+    }
+    for (int i = 0; i < N; i++) {
+        if (a.size() == 0) {
+            a.push(i);
+        }
+        else {
+            while (1) {
+                if (a.size() == 0)break;
+                if (t[a.top()][0] >= t[i][0])break;
+                else if (t[a.top()][0] < t[i][0]) {
+                    t[a.top()][1] = t[i][0];
+                    a.pop();
+                }
+            }
+            a.push(i);
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        if (t[i][1] == 0)t[i][1]--;
+        cout << t[i][1] << " ";
+    }
+}
+
---
+++ baekjoon일반/baekjoon(1100_하얀 칸)/baekjoon(1100_하얀 칸)/source.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    char a;
+    int ans = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            cin >> a;
+            if ((i + j) % 2 == 0 && a == 'F') {
+                ans++;
+            }
+        }
+    }
+    cout << ans;
+}
+
