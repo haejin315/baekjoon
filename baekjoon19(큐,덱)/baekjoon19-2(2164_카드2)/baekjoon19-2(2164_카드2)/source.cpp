bcfb11242eca4bca325c126c4834550d222a7aba
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-04-03 19:32:40 +0900

    baekjoon step by step

---
+++ baekjoon19(큐,덱)/baekjoon19-2(2164_카드2)/baekjoon19-2(2164_카드2)/source.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <queue>    
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    int N, a;
+    cin >> N;
+    queue<int> b;
+    for (int i = 1; i <= N; i++) {
+        b.push(i);
+    }
+    while (b.size() > 1) {
+        b.pop();
+        a = b.front();
+        b.pop();
+        b.push(a);
+    }
+    cout << b.front();
+}
+
