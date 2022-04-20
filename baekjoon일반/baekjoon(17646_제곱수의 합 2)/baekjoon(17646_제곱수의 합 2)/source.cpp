9f9af928fc4f7145ecfb93d3f8c37aae3b29f5cb
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-04-20 16:10:14 +0900

    baekjoon step by step

---
+++ baekjoon일반/baekjoon(17646_제곱수의 합 2)/baekjoon(17646_제곱수의 합 2)/source.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    long long n, m , t1, t2, t3, t4;
+    cin >> m;
+    n = m;
+    while (n != 0) {
+        t1 = floor(sqrt(n));
+        n -= t1 * t1;
+        cout << t1 << " " << n << endl;
+    }
+
+    cout << "\n\n";
+
+    while (n != 0) {
+        t1 = floor(sqrt(n));
+        n -= t1 * t1;
+        cout << t1 << " " << n << endl;
+    }
+}
+
