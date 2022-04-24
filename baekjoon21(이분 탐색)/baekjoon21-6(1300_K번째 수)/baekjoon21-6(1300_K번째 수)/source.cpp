7766e67d1925ed1d2a56d2ff00821df6420141ca
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-04-24 16:06:38 +0900

    baekjoon step by step

---
+++ baekjoon21(이분 탐색)/baekjoon21-6(1300_K번째 수)/baekjoon21-6(1300_K번째 수)/source.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int count(int N, int num) {
+    int re = 0;
+    for (int i = 1; i <= N; i++) {
+        re += min(N, (int)num / i);
+    }
+    return re;
+}
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, k;
+    
+    cin >> N >> k;
+    long long begin, end, mid;
+    begin = 1;
+    end = 1000000000;
+    int ans;  
+    while (1) {
+        if (begin > end)break;
+        mid = (begin + end) / 2;
+        if (count(N, mid) >= k) {
+            end = mid - 1;
+            ans = mid;
+        }
+        else {
+            begin = mid + 1;
+        }
+    }
+    cout << ans;
+}
+
