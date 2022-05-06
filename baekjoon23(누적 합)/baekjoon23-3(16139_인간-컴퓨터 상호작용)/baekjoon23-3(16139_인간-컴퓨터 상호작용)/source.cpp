c50a3d29a7df89145059080114e748beb829c398
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-05-06 23:53:26 +0900

    baekjoon step by step

---
+++ baekjoon23(누적 합)/baekjoon23-3(16139_인간-컴퓨터 상호작용)/baekjoon23-3(16139_인간-컴퓨터 상호작용)/source.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Count[200010][26] = { 0 };
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    string arr;
+    int q, begin, end;
+    char target;
+    cin >> arr >> q;
+    Count[1][arr[0] - 97]++;
+    for (int i = 1; i < arr.length(); i++) {
+        for (int j = 0; j < 26; j++) {
+            Count[i + 1][j] = Count[i][j];
+        }
+        Count[i + 1][arr[i] - 97] = Count[i][arr[i] - 97] + 1;
+    }
+
+    for (int i = 0; i < q; i++) {
+        cin >> target >> begin >> end;
+        cout << Count[end + 1][target - 97] - Count[begin][target - 97] << "\n";
+    }
+
+}
+
---
+++ baekjoon일반/baekjoon(1252_이진수 덧셈)/baekjoon(1252_이진수 덧셈)/source.cpp
@@ -0,0 +1,63 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    string a, b;
+    int c[100], z = 0, z2 = 0;
+    cin >> a >> b;
+    for (int i = 0; i < a.length(); i++) {
+        if (a[i] == '0')z++;
+        else break;
+    }
+    a = a.substr(z, a.length());
+    for (int i = 0; i < b.length(); i++) {
+        if (b[i] == '0')z2++;
+        else break;
+    }
+    b = b.substr(z2, b.length());
+
+    int d = a.length() - 1, e = b.length() - 1,f = 0, check = 0;
+    while (1) {
+        if (d < 0 && e < 0)break;
+        int g, h;
+        if (d < 0)g = 0;
+        else g = a[d] - 48;
+        if (e < 0)h = 0;
+        else h = b[e] - 48;
+        if (g == 1 && h == 1) {
+            c[f] = 0 + check;
+            check = 1;
+        }
+        else if (g == 0 && h == 0) {
+            c[f] = 0 + check;
+            check = 0;
+        }
+        else {
+            if (check == 0) {
+                c[f] = 1;
+                check = 0;
+            }
+            else {
+                c[f] = 0;
+                check = 1;
+            }
+        }
+        f++;
+        d--;
+        e--;
+    }
+    if (check == 1)cout << 1;
+    if (a.length() == 0 && b.length() == 0)cout << 0;
+    for (int i = f - 1; i >= 0; i--) {
+        cout << c[i];
+    }
+
+}
+
