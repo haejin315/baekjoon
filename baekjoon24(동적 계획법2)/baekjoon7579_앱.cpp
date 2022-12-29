65cdf66bf969b626d1627aff7adaa420e8176d44
haejin315 <64910494+haejin315@users.noreply.github.com>
2022-12-29 19:57:57 +0900

    Create baekjoon7579_앱.cpp

---
+++ baekjoon24(동적 계획법2)/baekjoon7579_앱.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[101][2] = { 0 };
+int b[101][10001] = { 0 };
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, M, s = 0;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        cin >> a[i][0];
+    }
+    for (int i = 1; i <= N; i++) {
+        cin >> a[i][1];
+
+        s += a[i][1];
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= s; j++) {
+            if (j >= a[i][1]) {
+                b[i][j] = max(b[i - 1][j], b[i - 1][j - a[i][1]] + a[i][0]);
+            }
+            else {
+                b[i][j] = b[i - 1][j];
+            }
+        }
+    }
+    for (int i = 0; i <= s; i++) {
+        if (b[N][i] >= M) {
+            cout << i;
+            break;
+        }
+    }
+}
+
