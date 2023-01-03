920b54dde52da0b152c74b223dcc79b363b9cee8
haejin315 <64910494+haejin315@users.noreply.github.com>
2023-01-03 22:34:15 +0900

    Create baekjoon24479_깊이 우선 탐색1.cpp

---
+++ baekjoon그래프와 순회/baekjoon24479_깊이 우선 탐색1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int curNode[200001] = { 0 };
+int c = 1;
+
+vector<int> v[100001];
+
+
+bool compare(pair<int, int>a, pair<int, int>b) {
+    if (a.second == b.second) {
+        return a.first < b.first;
+    }
+    return a.second < b.second;
+}
+
+void dfs(int R) {
+    if (curNode[R] != 0)return;
+    curNode[R] = c;
+    c++;
+    for (int i = 0; i < v[R].size(); i++) {
+        dfs(v[R][i]);
+    }
+}
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, M, R;
+    int a, b;
+    cin >> N >> M >> R;
+
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    for (int i = 0; i < N; i++) {
+        if (v[i + 1].size() == 0)continue;
+        sort(v[i + 1].begin(), v[i + 1].end());
+    }
+    dfs(R);
+    for (int i = 0; i < N; i++) {
+        cout << curNode[i + 1] << "\n";
+    }
+}
+
