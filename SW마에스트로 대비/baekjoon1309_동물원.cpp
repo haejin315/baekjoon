7c6ec8790523a888f27a4e889a03769d48ac1bc9
haejin315 <64910494+haejin315@users.noreply.github.com>
2023-02-16 19:26:01 +0900

    SW마에스트로

---
+++ SW마에스트로 대비/baekjoon1309_동물원.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int lion[100001] = { 0 };
+
+int main()
+{
+    int N, s = 3;
+    cin >> N;
+    int a[100001] = { 0 };
+    a[1] = 1;
+    for (int i = 2; i <= N; i++) {
+        a[i] = (s - a[i - 1]) % 9901;
+        s = (s + 2 * a[i]) % 9901;
+    }
+    cout << (s + 9901) % 9901;
+}
+
---
+++ SW마에스트로 대비/baekjoon2225_합분해.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    long long a[201][201] = {0};
+
+    for (int i = 0; i <= K; i++) {
+        a[i][0] = 1;
+    }
+    for (int i = 1; i <= K; i++) {
+        for (int j = 1; j <= N; j++) {
+            for (int k = 0; k <= j; k++) {
+                a[i][j] = (a[i][j] + a[i - 1][k]) % 1000000000;
+            }
+        }
+    }
+    cout << a[K][N];
+}
+
