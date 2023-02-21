71400ea18b8f0b0ac5d3f44d622f74736c32b230
haejin315 <64910494+haejin315@users.noreply.github.com>
2023-02-22 02:06:24 +0900

    SW마에스트로

---
+++ SW마에스트로 대비/baekjoon11727_2xn 타일링 2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstring>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+void aa(int s[3]) {
+    cout << s[0];
+    s[0] = 5;
+}
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    int a[1001] = { 0, 1, 3, 5 };
+
+    for (int i = 4; i <= n; i++) {
+        a[i] = (a[i - 2] * 2 + a[i - 1])%10007;
+    }
+    cout << a[n];
+}
+
